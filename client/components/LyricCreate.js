50a0163a3404e5e80e0e03b13bbe08d56055105f
AhmadAzadzadeh <a.azadzadeh.aa14@gmail.com>
2018-02-02 19:42:20 +0330

    Finish EveryThing About SongDetailComponent(All Component inside That)

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -1,14 +1,50 @@
 import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
 
 class LyricCreate extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { content: "" };
+	}
+	onSubmit(event) {
+		event.preventDefault();
+		this.props.mutate({
+			variables: {
+				content: this.state.content,
+				songId: this.props.id
+			}
+		}).then(() => {
+			this.setState({ content: "" });
+		});
+	}
 	render() {
 		return (
-			<form>
+			<form onSubmit={this.onSubmit.bind(this)}>
 				<label>Add a Lyric</label>
-				<input />
+				<input
+					value={this.state.content}
+					onChange={event => this.setState({ content: event.target.value })}
+				/>
+				<button className="btn-large green" type="submit">
+					Submit
+				</button>
 			</form>
 		);
 	}
 }
 
-export default LyricCreate;
+const mutation = gql`
+	mutation AddLyricToSong($content: String, $songId: ID) {
+		addLyricToSong(content: $content, songId: $songId) {
+			id
+			lyrics {
+				id
+				content
+				likes
+			}
+		}
+	}
+`;
+
+export default graphql(mutation)(LyricCreate);
